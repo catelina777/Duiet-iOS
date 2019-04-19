11915ade7e0679321c80d28c543b64e61530afa5
Ryuhei Kaminishi <catelina0717@gmail.com>
2019-04-20 00:14:05 +0900

    💄 Implement a protocol to customize the navigationbar

---
+++ Duiet/Sources/Common/Protocol/NavigationBarCustomizable.swift
@@ -0,0 +1,26 @@
+//
+//  NavigationBarCustomizable.swift
+//  Duiet
+//
+//  Created by 上西 隆平 on 2019/04/19.
+//  Copyright © 2019 duiet. All rights reserved.
+//
+
+import UIKit
+
+protocol NavigationBarCustomizable {
+    func configureNavigationBar(with title: String)
+}
+
+extension NavigationBarCustomizable where Self: UIViewController {
+
+    func configureNavigationBar(with title: String) {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.tintColor = #colorLiteral(red: 0.2418628931, green: 0.2533941567, blue: 0.3443268239, alpha: 1)
+        let largeTitleFont = R.font.montserratExtraBold(size: 32)!
+        let titleText = R.font.montserratExtraBold(size: 18)!
+        navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.font: largeTitleFont]
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: titleText]
+        self.title = title
+    }
+}
