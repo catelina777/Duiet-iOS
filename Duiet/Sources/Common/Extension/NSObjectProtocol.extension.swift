1ffac7dd3c6189d48b459e7f399491c60b299a76
Ryuhei Kaminishi <catelina0717@gmail.com>
2019-04-20 00:24:14 +0900

    🎨 Add function to get the class name

---
+++ Duiet/Sources/Common/Extension/NSObjectProtocol.extension.swift
@@ -0,0 +1,15 @@
+//
+//  NSObjectProtocol.extension.swift
+//  Duiet
+//
+//  Created by 上西 隆平 on 2019/04/19.
+//  Copyright © 2019 duiet. All rights reserved.
+//
+
+import Foundation
+
+extension NSObjectProtocol {
+    static var className: String {
+        return String(describing: self)
+    }
+}
