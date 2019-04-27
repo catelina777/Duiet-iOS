aec7d83720853071dcfbacd696cc3b29947889fb
Ryuhei Kaminishi <catelina0717@gmail.com>
2019-04-28 01:46:58 +0900

    💄 Add cell to input age

---
+++ Duiet/Sources/Common/Enum/CellType.swift
@@ -0,0 +1,18 @@
+//
+//  CellType.swift
+//  Duiet
+//
+//  Created by Ryuhei Kaminishi on 2019/04/27.
+//  Copyright © 2019 duiet. All rights reserved.
+//
+
+import Foundation
+
+enum CellType: String {
+    case gender = "Gender"
+    case age = "Age"
+    case height = "Height(cm)"
+    case weight = "Weight(kg)"
+    case activityLevel = "Activity Level"
+    case complete
+}
--- Duiet/Sources/UI/Common/RxTableViewCell.swift
+++ Duiet/Sources/UI/Common/RxTableViewCell.swift
@@ -9,14 +9,6 @@
 import UIKit
 import RxSwift
 
-enum CellType: String {
-    case gender = "Gender"
-    case height = "Height(cm)"
-    case weight = "Weight(kg)"
-    case activityLevel = "Activity Level"
-    case complete
-}
-
 class RxTableViewCell: UITableViewCell {
 
     var disposeBag = DisposeBag()
