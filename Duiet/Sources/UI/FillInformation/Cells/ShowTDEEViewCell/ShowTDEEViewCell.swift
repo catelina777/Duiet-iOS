2d968d0a7036913d34e0641b849d4f8fec541931
Ryuhei Kaminishi <catelina0717@gmail.com>
2019-04-28 01:44:20 +0900

    💄 Add UI to show BMR and TDEE

---
+++ Duiet/Sources/UI/FillInformation/Cells/ShowTDEEViewCell/ShowTDEEViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ShowTDEEViewCell.swift
+//  Duiet
+//
+//  Created by Ryuhei Kaminishi on 2019/04/27.
+//  Copyright © 2019 duiet. All rights reserved.
+//
+
+import UIKit
+
+final class ShowTDEEViewCell: RxTableViewCell {
+
+    @IBOutlet weak var BMRLabel: UILabel!
+    @IBOutlet weak var TDEELabel: UILabel!
+
+    func configure(with viewModel: FillInformationViewModel) {
+        viewModel.output.BMR
+            .bind(to: BMRLabel.rx.text)
+            .disposed(by: disposeBag)
+
+        viewModel.output.TDEE
+            .bind(to: TDEELabel.rx.text)
+            .disposed(by: disposeBag)
+    }
+}
