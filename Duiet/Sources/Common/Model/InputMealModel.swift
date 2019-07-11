3d458085e17c359c06fcbb0f83946c4262504179
RYUHEI KAMINISHI <catelina0717@gmail.com>
2019-07-11 21:07:54 +0900

    🎨 Refactoring

---
+++ Duiet/Sources/Common/Model/InputMealModel.swift
@@ -0,0 +1,122 @@
+//
+//  InputMealModel.swift
+//  Duiet
+//
+//  Created by 上西 隆平 on 2019/07/11.
+//  Copyright © 2019 duiet. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+import RxSwift
+import RxRelay
+import RxCocoa
+import RxRealm
+
+protocol InputMealModelProtocol {
+    var changeData: PublishRelay<RealmChangeset?> { get }
+    var contentDidUpdate: PublishRelay<Content> { get }
+    var contentDidDelete: PublishRelay<Void> { get }
+    var meals: PublishRelay<[Meal]> { get }
+    var day: BehaviorRelay<Day> { get }
+    var addMeal: Binder<Meal> { get }
+    var addContent: Binder<(Meal, Content)> { get }
+    var saveName: Binder<(Content, String)> { get }
+    var saveCalorie: Binder<(Content, Double)> { get }
+    var saveMultiple: Binder<(Content, Double)> { get }
+    var deleteContent: Binder<(Meal, Content)> { get }
+}
+
+final class InputMealModel: InputMealModelProtocol {
+
+    let meal = BehaviorRelay<Meal>(value: Meal(imagePath: ""))
+    let meals = PublishRelay<[Meal]>()
+    let day = BehaviorRelay<Day>(value: Day(date: Date()))
+    let contentDidDelete = PublishRelay<Void>()
+    let contentDidUpdate = PublishRelay<Content>()
+    let changeData = PublishRelay<RealmChangeset?>()
+
+    private let repository: DayRepositoryProtocol
+
+    private let disposeBag = DisposeBag()
+
+    init(date: Date = Date(),
+         repository: DayRepositoryProtocol) {
+        self.repository = repository
+    }
+
+    deinit {
+        print("InputMealModel Parge")
+    }
+
+    var addMeal: Binder<Meal> {
+        return Binder(self) { me, meal in
+            me.repository.add(meal: meal, to: me.day.value)
+        }
+    }
+
+    var addContent: Binder<(Meal, Content)> {
+        return Binder(self) { me, tuple in
+            me.repository.add(content: tuple.1, to: tuple.0)
+        }
+    }
+
+    var saveName: Binder<(Content, String)> {
+        return Binder(self) { me, tuple in
+            let content = tuple.0
+            me.repository.update(name: tuple.1, of: content)
+            me.contentDidUpdate.accept(content)
+        }
+    }
+
+    var saveCalorie: Binder<(Content, Double)> {
+        return Binder(self) { me, tuple in
+            let content = tuple.0
+            me.repository.update(calorie: tuple.1, of: content)
+            me.contentDidUpdate.accept(content)
+        }
+    }
+
+    var saveMultiple: Binder<(Content, Double)> {
+        return Binder(self) { me, tuple in
+            let content = tuple.0
+            me.repository.update(multiple: tuple.1, of: content)
+            me.contentDidUpdate.accept(content)
+        }
+    }
+
+    var deleteContent: Binder<(Meal, Content)> {
+        return Binder(self) { me, tuple in
+            me.repository.delete(content: tuple.1, of: tuple.0)
+            me.contentDidDelete.accept(())
+        }
+    }
+
+    private func observe(day: Day) {
+        Observable.from(object: day)
+            .subscribe(onNext: { [weak self] day in
+                guard let self = self else { return }
+                self.day.accept(day)
+            })
+            .disposed(by: disposeBag)
+    }
+
+    private func observe(mealResultsChangeset mealResults: Results<Meal>) {
+        Observable.changeset(from: mealResults)
+            .map { $1 }
+            .subscribe(onNext: { [weak self] changeset in
+                guard let self = self else { return }
+                self.changeData.accept(changeset)
+            })
+            .disposed(by: disposeBag)
+    }
+
+    private func observe(mealResults: Results<Meal>) {
+        Observable.array(from: mealResults)
+            .subscribe(onNext: { [weak self] meals in
+                guard let self = self else { return }
+                self.meals.accept(meals)
+            })
+            .disposed(by: self.disposeBag)
+    }
+}
---
+++ Duiet/Sources/UI/Day/InputMeal/NewInputMealViewModel.swift
@@ -0,0 +1,89 @@
+//
+//  NewInputMealViewModel.swift
+//  Duiet
+//
+//  Created by 上西 隆平 on 2019/07/11.
+//  Copyright © 2019 duiet. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxRelay
+
+final class NewInputMealViewModel {
+
+    let input: Input
+    let output: Output
+
+    private let inputMealModel: InputMealModelProtocol
+    private let coordinator: DayCoordinator
+    private let disposeBag = DisposeBag()
+
+    init(coordinator: DayCoordinator,
+         model: InputMealModelProtocol) {
+        self.coordinator = coordinator
+        self.inputMealModel = model
+
+        let _nameTextInput = PublishRelay<String?>()
+        let _calorieTextInput = PublishRelay<String?>()
+        let _multipleTextInput = PublishRelay<String?>()
+        let _selectedMealLabelViewModel = PublishRelay<MealLabelViewModel>()
+        let _contentWillDelete = PublishRelay<Void>()
+
+        input = Input(nameTextInput: _nameTextInput.asObserver(),
+                      calorieTextInput: _calorieTextInput.asObserver(),
+                      multipleTextInput: _multipleTextInput.asObserver(),
+                      selectedMealLabelViewModel: _selectedMealLabelViewModel.asObserver(),
+                      contentWillDelete: _contentWillDelete.asObserver())
+
+        let calorie = _calorieTextInput
+            .compactMap { $0 }
+            .map { Double($0) ?? 0 }
+            .distinctUntilChanged()
+            .share()
+
+        calorie.withLatestFrom(_selectedMealLabelViewModel) { ($1.content, $0) }
+            .bind(to: model.saveCalorie)
+            .disposed(by: disposeBag)
+
+        let multiple = _multipleTextInput
+            .compactMap { $0 }
+            .map { Double($0) ?? 0 }
+            .distinctUntilChanged()
+            .share()
+
+        multiple.withLatestFrom(_selectedMealLabelViewModel) { ($1.content, $0) }
+            .bind(to: model.saveMultiple)
+            .disposed(by: disposeBag)
+
+        let name = _nameTextInput
+            .distinctUntilChanged()
+            .map { $0 ?? "" }
+
+        name.withLatestFrom(_selectedMealLabelViewModel) { ($1.content, $0) }
+            .bind(to: model.saveName)
+            .disposed(by: disposeBag)
+
+        output = Output(contentDidUpdate: model.contentDidUpdate.asObservable(),
+                        contentDidDelete: model.contentDidDelete.asObservable())
+    }
+
+    deinit {
+        print("🧹🧹🧹 NewInputMealViewModel parge 🧹🧹🧹")
+    }
+}
+
+extension NewInputMealViewModel {
+
+    struct Input {
+        let nameTextInput: AnyObserver<String?>
+        let calorieTextInput: AnyObserver<String?>
+        let multipleTextInput: AnyObserver<String?>
+        let selectedMealLabelViewModel: AnyObserver<MealLabelViewModel>
+        let contentWillDelete: AnyObserver<Void>
+    }
+    struct Output {
+        let contentDidUpdate: Observable<Content>
+        let contentDidDelete: Observable<Void>
+    }
+}
