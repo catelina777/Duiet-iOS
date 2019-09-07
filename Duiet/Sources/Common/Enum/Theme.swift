2a45bf832e5586e90a0970c10188a7b7746d3dfc
RYUHEI KAMINISHI <catelina0717@gmail.com>
2019-09-07 20:03:29 +0900

    💄 Add color of navigationBarBarTintColor

--- Duiet/Sources/Common/Enum/Theme.swift
+++ Duiet/Sources/Common/Enum/Theme.swift
@@ -18,6 +18,7 @@ protocol Theme {
     var backgroundMainColor: UIColor { get }
     var tabBarTintColor: UIColor { get }
     var navigationBarTintColor: UIColor { get }
+    var navigationBarBarTintColor: UIColor { get }
     var cellHighlightColor: UIColor { get }
     var progressExceedColor: UIColor { get }
     var progressLessColor: UIColor { get }
@@ -26,33 +27,35 @@ protocol Theme {
 }
 
 struct LightTheme: Theme {
-    let titleColor             = UIColor(hex: "3E4158", alpha: 1)
-    let textMainColor          = UIColor(hex: "FFFFFF", alpha: 1)
-    let buttonMainColor        = UIColor(hex: "007AFF", alpha: 1)
-    let buttonDisableColor     = UIColor(hex: "999999", alpha: 1)
-    let backgroundMainColor    = UIColor(hex: "F2F2F2", alpha: 1)
-    let tabBarTintColor        = UIColor(hex: "FFFFFF", alpha: 1)
-    let navigationBarTintColor = UIColor(hex: "FFFFFF", alpha: 1)
-    let cellHighlightColor     = UIColor(hex: "00BAFF", alpha: 0.08)
-    let progressExceedColor    = UIColor(hex: "EE5787", alpha: 1)
-    let progressLessColor      = UIColor(hex: "007AFF", alpha: 1)
-    let progressNoneColor      = UIColor(hex: "E7E8E9", alpha: 1)
-    let statusBarStyle         = UIStatusBarStyle.default
+    let titleColor                = UIColor(hex: "3E4158", alpha: 1)
+    let textMainColor             = UIColor(hex: "FFFFFF", alpha: 1)
+    let buttonMainColor           = UIColor(hex: "007AFF", alpha: 1)
+    let buttonDisableColor        = UIColor(hex: "999999", alpha: 1)
+    let backgroundMainColor       = UIColor(hex: "F2F2F2", alpha: 1)
+    let tabBarTintColor           = UIColor(hex: "FFFFFF", alpha: 1)
+    let navigationBarTintColor    = UIColor(hex: "FFFFFF", alpha: 1)
+    let navigationBarBarTintColor = UIColor(hex: "3E4158", alpha: 1)
+    let cellHighlightColor        = UIColor(hex: "00BAFF", alpha: 0.08)
+    let progressExceedColor       = UIColor(hex: "EE5787", alpha: 1)
+    let progressLessColor         = UIColor(hex: "007AFF", alpha: 1)
+    let progressNoneColor         = UIColor(hex: "E7E8E9", alpha: 1)
+    let statusBarStyle            = UIStatusBarStyle.default
 }
 
 struct DarkTheme: Theme {
-    let titleColor             = UIColor(hex: "EEEEEE", alpha: 1)
-    let textMainColor          = UIColor(hex: "39C1BC", alpha: 1)
-    let buttonMainColor        = UIColor(hex: "39C1BC", alpha: 1)
-    let buttonDisableColor     = UIColor(hex: "8D8D8D", alpha: 1)
-    let backgroundMainColor    = UIColor(hex: "000000", alpha: 1)
-    let tabBarTintColor        = UIColor(hex: "000000", alpha: 1)
-    let navigationBarTintColor = UIColor(hex: "000000", alpha: 1)
-    let cellHighlightColor     = UIColor(hex: "39C1BC", alpha: 0.08)
-    let progressExceedColor    = UIColor(hex: "BF5F82", alpha: 1)
-    let progressLessColor      = UIColor(hex: "39C1BC", alpha: 1)
-    let progressNoneColor      = UIColor(hex: "2E3038", alpha: 1)
-    let statusBarStyle         = UIStatusBarStyle.lightContent
+    let titleColor                = UIColor(hex: "EEEEEE", alpha: 1)
+    let textMainColor             = UIColor(hex: "39C1BC", alpha: 1)
+    let buttonMainColor           = UIColor(hex: "39C1BC", alpha: 1)
+    let buttonDisableColor        = UIColor(hex: "8D8D8D", alpha: 1)
+    let backgroundMainColor       = UIColor(hex: "000000", alpha: 1)
+    let tabBarTintColor           = UIColor(hex: "000000", alpha: 1)
+    let navigationBarTintColor    = UIColor(hex: "EEEEEE", alpha: 1)
+    let navigationBarBarTintColor = UIColor(hex: "000000", alpha: 1)
+    let cellHighlightColor        = UIColor(hex: "39C1BC", alpha: 0.08)
+    let progressExceedColor       = UIColor(hex: "BF5F82", alpha: 1)
+    let progressLessColor         = UIColor(hex: "39C1BC", alpha: 1)
+    let progressNoneColor         = UIColor(hex: "2E3038", alpha: 1)
+    let statusBarStyle            = UIStatusBarStyle.lightContent
 }
 
 enum ThemeType: ThemeProvider {
