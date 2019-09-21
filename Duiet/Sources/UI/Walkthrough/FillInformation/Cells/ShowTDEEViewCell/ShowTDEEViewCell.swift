269ed0a52981f142bbbe977d2ae8c93d490aacbe
RYUHEI KAMINISHI <catelina0717@gmail.com>
2019-09-21 15:45:55 +0900

    💄 Localize fillInformationView

--- Duiet/Sources/UI/Walkthrough/FillInformation/Cells/ShowTDEEViewCell/ShowTDEEViewCell.swift
+++ Duiet/Sources/UI/Walkthrough/FillInformation/Cells/ShowTDEEViewCell/ShowTDEEViewCell.swift
@@ -9,8 +9,21 @@
 import UIKit
 
 final class ShowTDEEViewCell: BaseTableViewCell {
-    @IBOutlet weak var BMRValueLabel: UILabel!
-    @IBOutlet weak var TDEEValueLabel: UILabel!
+    @IBOutlet weak var BMRLabel: UILabel! {
+        didSet { BMRLabel.text = R.string.localizable.bmr() }
+    }
+
+    @IBOutlet weak var TDEELabel: UILabel! {
+        didSet { TDEELabel.text = R.string.localizable.tdee() }
+    }
+
+    @IBOutlet weak var BMRValueLabel: UILabel! {
+        didSet { BMRValueLabel.text = R.string.localizable.calc() }
+    }
+
+    @IBOutlet weak var TDEEValueLabel: UILabel! {
+        didSet { TDEEValueLabel.text = R.string.localizable.calc() }
+    }
 
     func configure(with viewModel: FillInformationViewModelProtocol) {
         viewModel.output.BMR
