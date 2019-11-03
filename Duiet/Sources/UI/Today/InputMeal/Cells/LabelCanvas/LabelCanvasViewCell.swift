b45531724742ebec081723345a17995ef4558662
RYUHEI KAMINISHI <catelina0717@gmail.com>
2019-11-03 22:58:51 +0900

    🎨 Refactor to reduce codes

--- Duiet/Sources/UI/Today/InputMeal/Cells/LabelCanvas/LabelCanvasViewCell.swift
+++ Duiet/Sources/UI/Today/InputMeal/Cells/LabelCanvas/LabelCanvasViewCell.swift
@@ -11,9 +11,6 @@ import RxGesture
 import RxSwift
 import UIKit
 
-/**
-    Immediately after the screen transition, display all saved labels only once
- */
 final class LabelCanvasViewCell: BaseTableViewCell {
     // TODO: This code is terrible and should be refactored
     func configure(input: InputMealViewModelInput, state: InputMealViewModelState) {
@@ -81,11 +78,7 @@ final class LabelCanvasViewCell: BaseTableViewCell {
     }
 
     private func configure(with image: UIImage?) {
-        let headerSize = CGRect(x: 0,
-                                y: 0,
-                                width: contentView.frame.width,
-                                height: contentView.frame.height)
-        let headerImageView = UIImageView(frame: headerSize)
+        let headerImageView = UIImageView(frame: contentView.frame)
         headerImageView.image = image
         headerImageView.clipsToBounds = true
         contentView.addSubview(headerImageView)
